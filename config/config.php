0a91dfdde64e153a70b9cb76b7c6d6eb0193b8bd
Andrew McMillan <debian@mcmillan.net.nz>
2006-10-08 00:29:28 +1300

    Misc irrelevant changes.

--- config/config.php
+++ config/config.php
@@ -8,7 +8,15 @@
   $c->pg_connect[] = 'dbname=caldav port=5433 user=general';
   $c->pg_connect[] = 'dbname=caldav port=5432 user=general';
 
-  $c->dbg['ALL'] = 1;
+//  $c->dbg['ALL'] = 1;
+  $c->dbg['propfind'] = 1;
+//  $c->dbg['report'] = 1;
+//  $c->dbg['get'] = 1;
+//  $c->dbg['put'] = 1;
+//  $c->dbg['ics'] = 1;
+//  $c->dbg['icalendar'] = 1;
+  $c->dbg['vevent'] = 1;
+  $c->dbg['caldav'] = 1;
 
   $c->collections_always_exist = false;
 
