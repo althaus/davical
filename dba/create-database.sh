e44888f7f8ade995c50febd5dd3e7a425b792ef3
Andrew McMillan <debian@mcmillan.net.nz>
2006-10-08 00:25:47 +1300

    Now ensure the plpgsql language is created.

--- dba/create-database.sh
+++ dba/create-database.sh
@@ -7,8 +7,16 @@ DBNAME="${1:-rscds}"
 
 DBADIR="`dirname \"$0\"`"
 
+# FIXME: Need to check that the database was actually created.
 createdb -E UTF8 "${DBNAME}"
 
+#
+# This will fail if the language already exists.
+# FIXME: test for the language first, perhaps.
+createlang plpgsql "${DBNAME}"
+
+#
+# FIXME: filter non-error output
 psql -f "${DBADIR}/rscds.sql" "${DBNAME}"
 
 psql -f "${DBADIR}/caldav_functions.sql" "${DBNAME}"
