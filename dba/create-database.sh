f4cc7b3d59f399a8201d2dda947c7b287ce419e5
Andrew McMillan <debian@mcmillan.net.nz>
2006-10-11 13:29:08 +1300

    More robust database creation script.

--- dba/create-database.sh
+++ dba/create-database.sh
@@ -8,17 +8,17 @@ DBNAME="${1:-rscds}"
 DBADIR="`dirname \"$0\"`"
 
 # FIXME: Need to check that the database was actually created.
-createdb -E UTF8 "${DBNAME}"
+createdb -E UTF8 "${DBNAME}" -T template0
 
 #
-# This will fail if the language already exists.
-# FIXME: test for the language first, perhaps.
+# This will fail if the language already exists, but it should not
+# because we created from template0.
 createlang plpgsql "${DBNAME}"
 
 #
 # FIXME: filter non-error output
-psql -f "${DBADIR}/rscds.sql" "${DBNAME}"
+psql -q -f "${DBADIR}/rscds.sql" "${DBNAME}" 2>&1 | egrep -v "(^CREATE |^GRANT|^BEGIN|^COMMIT| NOTICE: )"
 
-psql -f "${DBADIR}/caldav_functions.sql" "${DBNAME}"
+psql -q -f "${DBADIR}/caldav_functions.sql" "${DBNAME}"
 
-psql -f "${DBADIR}/sample-data.sql" "${DBNAME}"
+psql -q -f "${DBADIR}/sample-data.sql" "${DBNAME}"
