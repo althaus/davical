757e7c8b2f1d768710dc937303beda071b5769f4
Andrew McMillan <debian@mcmillan.net.nz>
2006-10-20 10:23:22 +1300

    Fix bug found when I first actually used it :-)

--- dba/update-database.sh
+++ dba/update-database.sh
@@ -11,7 +11,7 @@ DBADIR="`dirname \"$0\"`"
 # FIXME: We shouldn't really apply patches we already have installed, although
 # it should be safe enough since any patch which is not idempotent should be
 # denied multiple application through the AWL schema management functions.
-for PATCHFILE in "${DBADIR}/patches/*.*.*.sql"; do
+for PATCHFILE in ${DBADIR}/patches/*.*.*.sql ; do
   psql -q -f "${PATCHFILE}" "${DBNAME}"
 done
 
