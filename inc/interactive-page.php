6999306e5e2b346cd6ac27d485d46be7645ee55a
Andrew McMillan <debian@mcmillan.net.nz>
2006-10-29 10:47:11 +1300

    Add link to Sourceforge Bug creation page.

--- inc/interactive-page.php
+++ inc/interactive-page.php
@@ -4,6 +4,7 @@ $page_menu = new MenuSet('menu', 'menu', 'menu_active');
 $page_menu->AddOption("Home","/","Browse all users", false, 3900 );
 $page_menu->AddOption("Help","/help.php","Help on something or other", false, 4500 );
 $page_menu->AddOption("Logout","/?logout","Log out of the $c->system_name", false, 5400 );
+$page_menu->AddOption("Report Bug","http://sourceforge.net/tracker/?func=add&group_id=179845&atid=890785","Report a bug in the system", false, 9000 );
 
 $relationship_menu = new MenuSet('submenu', 'submenu', 'submenu_active');
 $user_menu = new MenuSet('submenu', 'submenu', 'submenu_active');
