449e231fb9170a2d2cb4174dd695186e6280bfca
Andrew McMillan <debian@mcmillan.net.nz>
2006-11-07 10:29:43 +1300

    This file is not included.  It just contains a number of strings that would be nice to be translated (as data, for example) which do not appear in the code.

---
+++ inc/other_translated_strings.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* This file is not included anywhere.  It exists as a way of ensuring some
+* strings, which may come from the database, or from other places, are
+* present in the translation files.
+*
+* It is also used to add extra entries to the translations which document
+* the sense of a word or phrase to assist with translations.
+*/
+
+// Groups, from the database
+i18n("Admin <!-- in the sense of 'systems admin' -->");
+i18n("Admin");
+
+i18n("Group <!-- in the sense of a group of people -->");
+i18n("Group");
+
+i18n("Public <!-- in the sense of being available to all users -->");
+i18n("Public");
+
+i18n("Resource <!-- a thing which might be booked: a room, a carpark, a projector... -->");
+i18n("Resource");
+
+i18n("Relationship <!-- in the sense of a work relationship between people -->");
+
+?>
