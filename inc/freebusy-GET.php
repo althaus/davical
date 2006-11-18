55406cc7a06ccc72ee1b53180fc7037788f339bc
Andrew McMillan <debian@mcmillan.net.nz>
2006-11-18 22:57:03 +1300

    Committing this placeholder for the time being.

---
+++ inc/freebusy-GET.php
@@ -0,0 +1,5 @@
+<?php
+  echo "<h1>Unimplemented</h1><p>Unfortunately this function is planned, but not yet implemented</p";
+  echo "<p>Well, actually, perhaps 'planned' is too strong a word.  It <em>is</em> on the roadmap, and if you want to
+contribute to the planning, please add your two cents to the Sourceforge forums or tracker items</p>";
+?>
