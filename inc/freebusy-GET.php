e576d884a33efb3c0c89a1c200a53c959a9d69e4
Andrew McMillan <debian@mcmillan.net.nz>
2006-11-22 20:58:18 +1300

    Switch to use new function for date presentation.

--- inc/freebusy-GET.php
+++ inc/freebusy-GET.php
@@ -1,18 +1,17 @@
 <?php
-
 require_once("iCalendar.php");
 
-  header("Content-type: text/plain");
-
-  $sql = "SELECT caldav_data.caldav_type AS type, calendar_item.uid, calendar_item.rrule ";
-  $sql .= ", to_char(dtstart at time zone 'UTC',".iCalendar::SqlUTCFormat().") AS dtstart ";
-  $sql .= ", to_char(dtend at time zone 'UTC',".iCalendar::SqlUTCFormat().") AS dtend ";
-  $sql .= ", to_char(due at time zone 'UTC',".iCalendar::SqlUTCFormat().") AS due ";
-  $sql .= ", to_char(dtstamp,".iCalendar::SqlUTCFormat().")  AS dtstamp ";
-  $sql .= ", to_char(last_modified,".iCalendar::SqlUTCFormat().")  AS \"last-modified\" ";
-  $sql .= " FROM caldav_data INNER JOIN calendar_item USING(user_no, dav_name)";
-  $sql .= " WHERE caldav_data.dav_name ~ ".qpg("^".$request_path)." ";
+  $sql = 'SELECT caldav_data.caldav_type AS type, calendar_item.uid, calendar_item.rrule ';
+  $sql .= ', to_ical_utc(dtstart)  AS dtstart ';
+  $sql .= ', to_ical_utc(dtend)    AS dtend ';
+  $sql .= ', to_ical_utc(due)      AS due ';
+  $sql .= ', to_ical_utc(dtstamp)  AS dtstamp ';
+  $sql .= ', to_ical_utc(last_modified)  AS "last-modified" ';
+  $sql .= ' FROM caldav_data INNER JOIN calendar_item USING(user_no, dav_name) ';
+  $sql .= ' WHERE caldav_data.dav_name ~ '.qpg("^".$request_path);
   $qry = new PgQuery( $sql );
+
+  header("Content-type: text/calendar");
 
   echo iCalendar::iCalHeader();
 
@@ -25,4 +24,5 @@ require_once("iCalendar.php");
     }
   }
   echo iCalendar::iCalFooter();
+
 ?>
