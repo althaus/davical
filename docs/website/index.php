40269c999cbd54c7b3f38e9aebbec9b01bec25b1
Andrew McMillan <debian@mcmillan.net.nz>
2006-11-30 20:46:31 +1300

    Go for the royal 'we' here, since this won't always be a one-person project.

--- docs/website/index.php
+++ docs/website/index.php
@@ -3,17 +3,18 @@
  include("inc/page-header.php");
 ?>
 <h1>Background</h1>
-<p>The CalDAV specification has been under development for a few years now, and at the same time I
-have seen increasing pressure from clients to provide a solution to their shared calendaring problems.
+<p>The CalDAV specification has been under development for a few years now, and at the same time we
+have seen increasing pressure from people and organisations in the open source world to provide a
+solution to their shared calendaring problems.
 </p>
 <p>In evaluating the possibilities for shared calendaring, there are a number of possible approaches, but
-I have elected to follow the path of implementing CalDAV because I believe it is a good specification
+we have elected to follow the path of implementing CalDAV because we believe it is a good specification
 and that it will in due course gain client implementations and provide the richest user
 experience through those client implementations.</p>
 
 <h1>Goals</h1>
 <p>CalDAV is a client-server protocol specific to managing and reporting on <em>collections</em> of <em>calendar resources</em>.</p>
-<p>As such, my intentions in developing this application are as follows:</p>
+<p>As such, our intentions in developing this application are as follows:</p>
 <ul>
 <li>Simplicity of Prerequisites</li>
 <li>Simplicity of Setup</li>
@@ -22,16 +23,18 @@ experience through those client implementations.</p>
 </ul>
 
 <h2>Simplicity of Prerequisites</h2>
-<p>I have chosen to write this in PHP because I believe that PHP is a widely available web scripting language.</p>
-<p>I have chosen to use the Apache web server because it is also widely available.  This is not necessarily a requirement,
-but I do not have other PHP environments available to me at this time.</p>
-<p>I have chosen to use the PostgreSQL database, because it is a free, open-source database, which operates on a very wide set of
+<p>We have chosen to write this in PHP because we believe that PHP is a widely available web scripting language.</p>
+<p>We have chosen to use the Apache web server because it is also widely available.  This is not necessarily a requirement,
+but no testing has been undertaken in other PHP environments to date.</p>
+<p>We have chosen to use the PostgreSQL database, because it is a free, open-source database, which operates on a very wide set of
 operating environments, and which is <em>fully</em> ACID compliant.</p>
 
 <h2>Simplicity of Setup</h2>
-<p>I use the <a href="http://www.debian.org/">Debian GNU/Linux</a> distribution, so at this stage I have made Debian packages available.  If
-I find willing people I will make packages available in other forms.</p>
-<p>This goal is not expected to be achieved in the first few releases.</p>
+<p>For the greatest ease use you should consider installing RSCDS on the <a href="http://www.debian.org/">Debian GNU/Linux</a>
+distribution from the readily available, signed packages.</p>
+<p>We expect to increase the level of automation and simplicity for the Debian target release in particular, although other
+distributions might also become easier at the same time. We do expect slightly greater installation complexity in the first
+few releases as we come to understand the particular problems people experience.</p>
 
 <h2>Simplicity of Operation</h2>
 <p>In general RSCDS should not need significant maintenance to keep it operating.</p>
@@ -60,12 +63,13 @@ works using the CalDAV protocol.</p>
 
 <h1>Your Name Here!</h1>
 <p>If you are interested in helping, there are several areas where I need help at the moment:</p>
-<ul>
+<i>
 <li>The project needs a better name - feel free to suggest one!</li>
 <li>We need more documentation</li>
 <li>The graphic and UI design for the web-based administration is teh suck (but it is basically functional :-)</li>
 <li>We need more documentation</li>
 <li>I need to find more CalDAV-capable calendar clients to interoperate with</li>
+<li>We would love you to write about your experiences in the project Wiki.</li>
 </ul>
 
 <?php
