a972fc8439f704f22d5ba920cf637993b3126f9c
Andrew McMillan <debian@mcmillan.net.nz>
2006-12-28 17:01:51 +1300

    Fixed upgrade patch.

--- dba/patches/1.1.7.sql
+++ dba/patches/1.1.7.sql
@@ -8,6 +8,16 @@ ALTER TABLE relationship_type DROP COLUMN rt_inverse;
 ALTER TABLE relationship_type DROP COLUMN prefix_match;
 ALTER TABLE relationship_type DROP COLUMN rt_isgroup;
 
+UPDATE relationship_type SET rt_name ='Administers', confers = 'A' WHERE rt_id = 1;
+UPDATE relationship_type SET rt_name ='is Assistant to', confers = 'RW' WHERE rt_id = 2;
+UPDATE relationship_type SET rt_name ='Can read from', confers = 'R' WHERE rt_id = 3;
+UPDATE relationship_type SET rt_name ='Can see free/busy time of', confers = 'F' WHERE rt_id = 4;
+
+UPDATE relationship SET rt_id=1 WHERE rt_id=4;
+UPDATE relationship SET rt_id=4 WHERE rt_id=5;
+
+DELETE FROM relationship_type WHERE rt_id = 5;
+
 SELECT new_db_revision(1,1,7, 'July' );
 COMMIT;
 ROLLBACK;
