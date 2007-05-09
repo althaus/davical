2598e489bb7e7d7211451b8e0dbeb1e2d5292a35
Andrew McMillan <debian@mcmillan.net.nz>
2007-05-09 14:10:23 +1200

    Ensure username comparison is fully case-insensitive.

--- inc/BasicAuthSession.php
+++ inc/BasicAuthSession.php
@@ -90,7 +90,7 @@ class BasicAuthSession {
       return call_user_func( $c->authenticate_hook['call'], $username, $password );
     }
 
-    $qry = new PgQuery( "SELECT * FROM usr WHERE lower(username) = ? ", $username );
+    $qry = new PgQuery( "SELECT * FROM usr WHERE lower(username) = lower(?) ", $username );
     if ( $qry->Exec('BasicAuth',__LINE__,__FILE__) && $qry->rows == 1 ) {
       $usr = $qry->Fetch();
       dbg_error_log( "BasicAuth", ":CheckPassword: Name:%s, Pass:%s, File:%s", $username, $password, $usr->password );
