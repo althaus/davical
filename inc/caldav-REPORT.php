daab2bc748e904adeefd721a9cb3a5eb17392d86
Andrew McMillan <debian@mcmillan.net.nz>
2007-08-26 11:23:00 +1200

    Refactor handling of requested properties in REPORT requests for better flexibility.

--- inc/caldav-REPORT.php
+++ inc/caldav-REPORT.php
@@ -59,58 +59,76 @@ function calendar_to_xml( $properties, $item ) {
 
   dbg_error_log("REPORT","Building XML Response for item '%s'", $item->dav_name );
 
-  $url = $c->protocol_server_port_script . $item->dav_name;
-  $prop = new XMLElement("prop");
-  if ( isset($properties['GETCONTENTLENGTH']) ) {
-    $contentlength = strlen($item->caldav_data);
-    $prop->NewElement("getcontentlength", $contentlength );
-  }
-  if ( isset($properties['CALENDAR-DATA']) ) {
+  $caldav_data = $item->caldav_data;
+  $displayname = $item->summary;
+  if ( isset($properties['CALENDAR-DATA']) || isset($properties['DISPLAYNAME']) ) {
     if ( !is_numeric(strpos($item->permissions,'A')) && $session->user_no != $item->user_no ){
       // the user is not admin / owner of this calendarlooking at his calendar and can not admin the other cal
       if ( $item->class == 'CONFIDENTIAL' ) {
         // if the event is confidential we fake one that just says "Busy"
+        $displayname = translate("Busy");
         $ical = new iCalendar( array( "icalendar" => $item->caldav_data) );
-        $ical->Put( 'SUMMARY', translate("Busy") );
+        $ical->Put( 'SUMMARY', $displayname );
         $caldav_data = $ical->render(true, $item->caldav_type, $ical->DefaultPropertyList() );
-        $prop->NewElement("calendar-data","$caldav_data" , array("xmlns" => "urn:ietf:params:xml:ns:caldav") );
       }
       elseif ( $c->hide_alarm ) {
         // Otherwise we hide the alarms (if configured to)
         $ical = new iCalendar( array( "icalendar" => $item->caldav_data) );
         $caldav_data = $ical->render(true, $item->caldav_type, $ical->DefaultPropertyList() );
-        $prop->NewElement("calendar-data","$caldav_data" , array("xmlns" => "urn:ietf:params:xml:ns:caldav") );
-      }
-      else {
-        // Just send the raw event
-        $prop->NewElement("calendar-data", $item->caldav_data, array("xmlns" => "urn:ietf:params:xml:ns:caldav") );
       }
     }
-    else
-      // Just send the raw event
-      $prop->NewElement("calendar-data", $item->caldav_data, array("xmlns" => "urn:ietf:params:xml:ns:caldav") );
   }
-  if ( isset($properties['GETCONTENTTYPE']) ) {
-    $prop->NewElement("getcontenttype", "text/calendar" );
-  }
-  if ( isset($properties['RESOURCETYPE']) ) {
-    $prop->NewElement("resourcetype", new XMLElement("calendar", false, array("xmlns" => "urn:ietf:params:xml:ns:caldav")) );
-  }
-  if ( isset($properties['DISPLAYNAME']) ) {
-    $prop->NewElement("displayname");
-  }
-  if ( isset($properties['GETETAG']) ) {
-    $prop->NewElement("getetag", '"'.$item->dav_etag.'"' );
-  }
-  if ( isset($properties['CURRENT-USER-PRIVILEGE-SET']) ) {
-    $prop->NewElement("current-user-privilege-set", privileges($request->permissions) );
+
+  $url = $c->protocol_server_port_script . $item->dav_name;
+  $prop = new XMLElement("prop");
+  foreach( $properties AS $k => $v ) {
+    switch( $k ) {
+      case 'GETCONTENTLENGTH':
+        $contentlength = strlen($item->caldav_data);
+        $prop->NewElement("getcontentlength", $contentlength );
+        break;
+      case 'CALENDAR-DATA':
+        $prop->NewElement("calendar-data","$caldav_data" , array("xmlns" => "urn:ietf:params:xml:ns:caldav") );
+        break;
+      case 'GETCONTENTTYPE':
+        $prop->NewElement("getcontenttype", "text/calendar" );
+        break;
+      case 'RESOURCETYPE':
+        $prop->NewElement("resourcetype", new XMLElement("calendar", false, array("xmlns" => "urn:ietf:params:xml:ns:caldav")) );
+        break;
+      case 'DISPLAYNAME':
+        $prop->NewElement("displayname", $displayname );
+        break;
+      case 'GETETAG':
+        $prop->NewElement("getetag", '"'.$item->dav_etag.'"' );
+        break;
+      case 'CURRENT-USER-PRIVILEGE-SET':
+        $prop->NewElement("current-user-privilege-set", privileges($request->permissions) );
+        break;
+      case 'SOME-DENIED-PROPERTY':  /** TODO: indicating the style for future expansion */
+        $denied[] = $v;
+        break;
+      default:
+        dbg_error_log( 'REPORT', "Request for unsupported property '%s' of calendar item.", $v );
+        $unsupported[] = $v;
+    }
   }
   $status = new XMLElement("status", "HTTP/1.1 200 OK" );
 
   $propstat = new XMLElement( "propstat", array( $prop, $status) );
   $href = new XMLElement("href", $url );
+  $elements = array($href,$propstat);
 
-  $response = new XMLElement( "response", array($href,$propstat));
+  if ( count($denied) > 0 ) {
+    $status = new XMLElement("status", "HTTP/1.1 403 Forbidden" );
+    $noprop = new XMLElement("prop");
+    foreach( $denied AS $k => $v ) {
+      $noprop->NewElement( strtolower($v) );
+    }
+    $elements[] = new XMLElement( "propstat", array( $noprop, $status) );
+  }
+
+  $response = new XMLElement( "response", $elements );
 
   return $response;
 }
