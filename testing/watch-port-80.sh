0732c60bb286c24dd3db567f80a4515ad0a0dcea
Andrew McMillan <debian@mcmillan.net.nz>
2007-11-05 22:58:01 +1300

    Allow the interface to be specified.

--- testing/watch-port-80.sh
+++ testing/watch-port-80.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 PORT=${1:-"80"}
+IFACE=${2:-"any"}
 
-sudo tcpdump -i any -s0 -l -n -q -A "tcp port ${PORT} and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)"
+sudo tcpdump -i $IFACE -s0 -l -n -q -A "tcp port ${PORT} and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)"
