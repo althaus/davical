7b04617c378961f8db0f4a7638890b2497b60c8f
Andrew McMillan <debian@mcmillan.net.nz>
2008-01-25 07:58:30 +1300

    Performance fixes to freebusy and results ordering change.

--- inc/freebusy-GET.php
+++ inc/freebusy-GET.php
@@ -19,7 +19,6 @@ else {
   $finish = date( "Ymd\THis", time() + (86400 * 200) );
 }
 
-
 if ( isset($request->by_email) ) {
   $where = "WHERE caldav_data.user_no = $request->user_no ";
 }
@@ -31,14 +30,17 @@ $where .= "AND dtstart <= '$finish'::timestamp with time zone ";
 $where .= "AND caldav_data.caldav_type IN ( 'VEVENT', 'VFREEBUSY' ) ";
 $where .= "AND (calendar_item.transp != 'TRANSPARENT' OR calendar_item.transp IS NULL) ";
 $where .= "AND (calendar_item.status != 'CANCELLED' OR calendar_item.status IS NULL) ";
-$where .= "AND (calendar_item.class != 'PRIVATE' OR calendar_item.class IS NULL OR get_permissions($session->user_no,caldav_data.user_no) ~ 'A') "; // Must have 'all' permissions to see confidential items
+if ( ! $request->AllowedTo('all') ) {
+  $where .= "AND (calendar_item.class != 'PRIVATE' OR calendar_item.class IS NULL) ";
+}
 
 $busy = array();
 $busy_tentative = array();
 $sql = "SELECT caldav_data.caldav_data, calendar_item.rrule, calendar_item.transp, calendar_item.status, ";
 $sql .= "to_char(calendar_item.dtstart at time zone 'GMT',".iCalendar::SqlDateFormat().") AS start, ";
 $sql .= "to_char(calendar_item.dtend at time zone 'GMT',".iCalendar::SqlDateFormat().") AS finish ";
-$sql .= "FROM caldav_data INNER JOIN calendar_item USING(user_no, dav_name) $where ORDER BY dtstart, dtend";
+$sql .= "FROM caldav_data INNER JOIN calendar_item USING(dav_id,user_no,dav_name)".$where;
+if ( isset($c->strict_result_ordering) && $c->strict_result_ordering ) $sql .= " ORDER BY dav_id";
 // echo $sql. "\n";
 $qry = new PgQuery( $sql );
 if ( $qry->Exec("freebusy",__LINE__,__FILE__) && $qry->rows > 0 ) {
