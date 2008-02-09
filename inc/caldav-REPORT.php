a059beb8331e75b48badce1070cdc145b3b9d3f3
Andrew McMillan <debian@mcmillan.net.nz>
2008-02-09 21:20:55 +1300

    When creating a fake event, don't create a blank RRULE.

--- inc/caldav-REPORT.php
+++ inc/caldav-REPORT.php
@@ -77,9 +77,10 @@ function calendar_to_xml( $properties, $item ) {
         // if the event is confidential we fake one that just says "Busy"
         $confidential = new iCalendar( array(
                               'SUMMARY' => translate('Busy'), 'CLASS' => 'CONFIDENTIAL',
-                              'DTSTART'  => $ical->Get('DTSTART'),
-                              'RRULE'    => $ical->Get('RRULE')
+                              'DTSTART' => $ical->Get('DTSTART')
                           ) );
+        $rrule = $ical->Get('RRULE');
+        if ( isset($rrule) && $rrule != '' ) $confidential->Set('RRULE', $rrule);
         $duration = $ical->Get('DURATION');
         if ( isset($duration) && $duration != "" ) {
           $confidential->Set('DURATION', $duration );
