e87ae9ac63d72859e0f3bfc7f25fa5bce5538969
Andrew McMillan <andrew@morphoss.com>
2010-09-14 13:50:27 +1200

    Release 0.9.9.1 for testing.

--- htdocs/always.php
+++ htdocs/always.php
@@ -166,7 +166,7 @@ init_gettext( 'davical', $c->locale_path );
 *
 */
 $c->code_version = 0;
-$c->version_string = '0.9.9'; // The actual version # is replaced into that during the build /release process
+$c->version_string = '0.9.9.1'; // The actual version # is replaced into that during the build /release process
 if ( isset($c->version_string) && preg_match( '/(\d+)\.(\d+)\.(\d+)(.*)/', $c->version_string, $matches) ) {
   $c->code_major = $matches[1];
   $c->code_minor = $matches[2];
