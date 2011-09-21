fe659b4aaa3f8d46359e5c448c25ba4b8a82b907
Andrew McMillan <andrew@morphoss.com>
2011-09-22 07:53:04 +1200

    Set 'active' and 'default_privileges' fallback values.

--- inc/drivers_ldap.php
+++ inc/drivers_ldap.php
@@ -261,7 +261,11 @@ function sync_user_from_LDAP( Principal &$principal, $mapping, $ldap_values ) {
 
   dbg_error_log( "LDAP", "Going to sync the user from LDAP" );
 
-  $fields_to_set = array( 'username' => $principal->username() );
+  $fields_to_set = array(
+   'username' => $principal->username(),
+   'active' => true,
+   'default_privileges' => decbin(privilege_to_bits($c->default_privileges))
+  );
   $updateable_fields = Principal::updateableFields();
   $updateable_fields[] = 'active';  // Backward compatibility: now 'user_exists'
   $updateable_fields[] = 'updated'; // Backward compatibility: now 'modified'
