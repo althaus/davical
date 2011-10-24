bb8bf75e8f2cdd6f1ae9f52b383e96f54feb76fc
Andrew McMillan <andrew@morphoss.com>
2011-10-24 20:27:43 +1300

    Release 0.9.9.7

--- htdocs/always.php
+++ htdocs/always.php
@@ -174,8 +174,8 @@ init_gettext( 'davical', $c->locale_path );
 *
 */
 $c->code_version = 0;
-$c->want_awl_version = '0.48';
-$c->version_string = '0.9.9.6'; // The actual version # is replaced into that during the build /release process
+$c->want_awl_version = '0.49';
+$c->version_string = '0.9.9.7'; // The actual version # is replaced into that during the build /release process
 if ( isset($c->version_string) && preg_match( '/(\d+)\.(\d+)\.(\d+)(.*)/', $c->version_string, $matches) ) {
   $c->code_major = $matches[1];
   $c->code_minor = $matches[2];
